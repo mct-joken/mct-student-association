659f4b42702c26f10e89a92e61dd9b8dee8338c4
kiji-haru <kijiharu3112@outlook.jp>
2023-04-21 00:43:13 +0900

    feat:prettierとeslintの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-      <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
-                <Route path="*" element={<h1>404</h1>} />
-            </Routes>
-        </BrowserRouter>
-      </>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
