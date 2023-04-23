ecc3a10687dd7222cb6134925054399b6fac7123
kiji-haru <kijiharu3112@outlook.jp>
2023-04-23 13:38:03 +0900

    feat:homeページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BrowserRouter>
