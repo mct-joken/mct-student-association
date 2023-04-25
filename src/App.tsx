f04bd8f884c4e4e52243ddc3c0b2ffa9ead05ad7
kiji-haru <kijiharu3112@outlook.jp>
2023-04-25 17:13:15 +0900

    feat:ルーター

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
+import {Header} from "./components/header";
+import "./App.css";
+import {Footer} from "./components/footer";
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    const [navOpen, setNavOpen] = React.useState<boolean>(false);
+    return (
+        <>
+            <BrowserRouter>
+                <Header setNavOpen={setNavOpen}/>
+                {!navOpen ?
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="*" element={<h1>404</h1>} />
+                    </Routes>
+                    : null}
+                <Footer/>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
