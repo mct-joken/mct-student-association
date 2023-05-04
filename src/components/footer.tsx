d7d3fe83a5321b825f2cb8ed823f7878669c8f2b
Tufusa <louptung@gmail.com>
2023-05-04 19:04:18 +0900

    refactor: footerをnarrowFooterとwideFooterに分割 スタイルはCSS Modulesで当てる

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,10 @@
-import React from "react";
+import { useMediaQuery } from "react-responsive"
 
-import { Link } from "react-router-dom";
-import { useMedia } from "use-media"
-
-import TwitterLogo from "../assets/TwitterLogo.svg";
-import InstagramLogo from "../assets/InstagramLogo.svg";
-import "../css/footer.css"
+import { WideFooter } from "./wideFooter";
+import { NarrowFooter } from "./narrowFooter";
 
 export function Footer() {
-    const isNarrow = useMedia({ maxWidth: "600px" });
-    return (
-        <footer className={`footer ${isNarrow ? "narrow" : ""}`}>
-            <div className="links">
-                <ul>
-                    <li><Link to={"/schedule"}>活動予定</Link></li>
-                    <li><Link to={"/club"}>部活動</Link></li>
-                    <li><Link to={"/members"}>学生会メンバー</Link></li>
-                    <li><Link to={"/links"} >リンク</Link></li>
-                    <li><Link to={"/industry"}>インダストリ</Link></li>
-                </ul>
-            </div>
-            {isNarrow &&
-                <div className="icons">
-                    <Link to={"https://twitter.com/MctGakuseikai"}><img src={TwitterLogo} alt={"松江高専学生会のTwitter"} /></Link>
-                    <Link to={"https://www.instagram.com/mctgakuseikai/"}><img src={InstagramLogo} alt={"松江高専学生会のInstagram"} /></Link>
-                </div>
-            }
-        </footer>
-    )
+    const isNarrow = useMediaQuery({ maxWidth: 600 })
+
+    return isNarrow ? <NarrowFooter /> : <WideFooter />
 }
---
+++ src/components/narrowFooter.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+import TwitterLogo from "../assets/TwitterLogo.svg";
+import InstagramLogo from "../assets/InstagramLogo.svg";
+import styles from "../css/narrowFooter.module.css"
+
+export const NarrowFooter = () => (
+  <footer className={styles.footer}>
+    <div className={styles.links}>
+      <ul className={styles.ul}>
+        <LinkItem to="/schedule" children="活動予定" />
+        <LinkItem to="/club"     children="部活動" />
+        <LinkItem to="/members"  children="学生会メンバー" />
+        <LinkItem to="/links"    children="リンク" />
+        <LinkItem to="/industry" children="インダストリ" />
+      </ul>
+    </div>
+    <div className={styles.icons}>
+      <LinkIcon
+        to="https://twitter.com/MctGakuseikai/"
+        src={TwitterLogo}
+        alt="松江高専学生会のTwitter"
+      />
+      <LinkIcon
+        to="https://www.instagram.com/mctgakuseikai/"
+        src={InstagramLogo}
+        alt="松江高専学生会のInstagram"
+      />
+    </div>
+  </footer>
+)
+
+const LinkItem = (props: { children: string, to: string }) => (
+  <li className={styles.li}>
+    <Link to={props.to} children={props.children} className={styles.link}/>
+  </li>
+)
+
+const LinkIcon = (props: { to: string, src: string, alt: string }) => (
+  <Link to={props.to}>
+    <img src={props.src} alt={props.alt} className={styles.icon} />
+  </Link>
+)
