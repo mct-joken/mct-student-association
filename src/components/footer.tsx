bcb5a22c54fce24e11dce59e8758693b086b8f8a
Tufusa <louptung@gmail.com>
2023-05-05 20:08:47 +0900

    refactor: prettier

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
-import { useMediaQuery } from "react-responsive"
+import { useMediaQuery } from 'react-responsive';
 
-import { WideFooter } from "./wideFooter";
-import { NarrowFooter } from "./narrowFooter";
+import { WideFooter } from './ResponsiveFooter/wideFooter';
+import { NarrowFooter } from './ResponsiveFooter/narrowFooter';
 
-export function Footer() {
-    const isNarrow = useMediaQuery({ maxWidth: 600 })
+export const Footer = () => {
+  const isNarrow = useMediaQuery({ maxWidth: 600 });
 
-    return isNarrow ? <NarrowFooter /> : <WideFooter />
-}
+  return isNarrow ? <NarrowFooter /> : <WideFooter />;
+};
