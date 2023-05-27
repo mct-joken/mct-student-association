130d6dbf81d977835fdcd1ef5303a239c105cdc3
kijiharu3112 <kijiharu3112@outlook.jp>
2023-05-27 16:12:45 +0900

    feat:#40 部活動のリンクをついか

--- src/pages/club.tsx
+++ src/pages/club.tsx
@@ -1,10 +1,11 @@
 import { Title } from '../components/title';
 import { Club_card } from '../components/club_card';
 import { Props } from '../components/club_card';
-
+import "../css/club.css";
 export const Club = () => (
-  <div className={"page"}>
+  <div className={"page"} id={"club"}>
     <Title title={'部活動'} />
+    <h3>体育局</h3>
     <div
       style={{
         flexWrap: 'wrap',
@@ -15,7 +16,8 @@ export const Club = () => (
         justifyContent: 'space-between',
       }}
     >
-      {clubs.map((club) => (
+
+      {athletic_club.map((club) => (
         <Club_card
           name={club.name}
           twitter={club.twitter}
@@ -26,46 +28,69 @@ export const Club = () => (
         />
       ))}
     </div>
+    <h3>文化局</h3>
+    <div
+      style={{
+        flexWrap: 'wrap',
+        display: 'flex',
+        width: '80%',
+        maxWidth: '800px',
+        minWidth: '300px',
+        justifyContent: 'space-between',
+      }}
+    >
+      {cultural_club.map((club) => (
+        <Club_card
+          name={club.name}
+          twitter={club.twitter}
+          instagram={club.instagram}
+          homepage={club.homepage}
+          facebook={club.facebook}
+          key={club.name}
+        />
+        ))}
+    </div>
   </div>
 );
-
-const clubs: Props[] = [
+const athletic_club: Props[] = [
   { name: '陸上競技部', twitter: 'https://twitter.com/matsue_ct_TF' },
   { name: '柔道部', twitter: 'https://twitter.com/Mct_judo20' },
   { name: '剣道部' },
   { name: 'サッカー部', twitter: 'https://twitter.com/mct_football' },
-  { name: '卓球部' },
+  { name: '卓球部', instagram: 'https://www.instagram.com/mct.tabletennis/' },
   { name: 'アーチェリー部' },
   { name: '弓道部' },
   { name: '硬式野球部' },
   { name: 'テニス部' },
-  { name: 'ラグビー部' },
+  { name: 'ラグビー部', instagram: 'https://www.instagram.com/matsue_rugby/',},
   { name: 'バスケットボール部', twitter: 'https://twitter.com/Matsuekosen_BB' },
+  { name: '女子バスケ部', instagram: 'https://twitter.com/manken_mct'},
   { name: 'ソフトテニス部' },
-  { name: 'ボート部', twitter: 'https://twitter.com/Matsuekosen_BB' },
+  { name: 'ボート部', twitter: 'https://twitter.com/Matsuekosen_BB', instagram: 'https://www.instagram.com/mctrt_official/' },
   { name: '水泳部', twitter: 'https://twitter.com/matsue_ct_swim' },
-  { name: 'ハンドボール部' },
+  { name: 'ハンドボール部', instagram: 'https://www.instagram.com/mct.handball/'},
   { name: 'バドミントン部' },
   { name: '男子バレーボール部' },
   { name: '女子バレーボール部' },
-  { name: '写真部', twitter: 'https://twitter.com/mct_shashinbu' },
+];
+const cultural_club: Props[] = [
+  { name: '写真部', twitter: 'https://twitter.com/mct_shashinbu', instagram: 'https://www.instagram.com/mct.photos/'},
   { name: '理科部' },
   { name: '社会奉仕部' },
   { name: '情報科学研究部', twitter: 'https://twitter.com/mct_joken', homepage: 'https://joken.dev' },
-  { name: '囲碁将棋部' },
-  { name: '郷土研究部', twitter: 'https://twitter.com/kyoudoken' },
-  { name: 'ロボコン部' },
+  { name: '囲碁将棋部', twitter: 'https://twitter.com/MCTIgoShogi', instagram: 'https://www.instagram.com/mct_shogi_club/' },
+  { name: '郷土研究部', twitter: 'https://twitter.com/kyoudoken', instagram: 'https://www.instagram.com/mct.kyodo_club/' },
+  { name: 'ロボコン部', twitter: 'https://twitter.com/Matsue_Robocon' },
   { name: 'エコラン' },
-  { name: '吹奏楽部', twitter: 'https://twitter.com/mctbb_prm', facebook: 'https://www.facebook.com/mctwind/' },
+  { name: '吹奏楽部', twitter: 'https://twitter.com/mctbb_prm', facebook: 'https://www.facebook.com/mctwind/', instagram: 'https://www.instagram.com/mct_windclub/' },
   { name: '文芸部' },
   { name: '書道部' },
   { name: 'ESS部' },
-  { name: '漫画研究同好会' },
+  { name: '漫画研究同好会', twitter: 'https://twitter.com/manken_mct'},
   { name: '美術同好会' },
   { name: '数学同好会' },
-  {
-    name: 'レスコン',
-    twitter: 'https://twitter.com/MCTrescon',
-    homepage: 'https://mctrescon.blogspot.com/2017/05/what-is-mct-mct-mct-matsue-college-of.html?m=1',
-  },
-];
+  { name: 'レスコン', twitter: 'https://twitter.com/MCTrescon', homepage: 'https://mctrescon.blogspot.com/2017/05/what-is-mct-mct-mct-matsue-college-of.html?m=1', },
+  { name: 'デザコン', twitter: 'https://twitter.com/mct_dezakon'},
+  { name: 'クイズ研究会', instagram: 'https://www.instagram.com/mctquiz/', twitter: 'https://twitter.com/MCT_Quiz'}
+
+]
