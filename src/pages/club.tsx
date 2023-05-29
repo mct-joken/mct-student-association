0d74fe5e52103a482f7b1cac128413ff86774961
Teselapfer <schwarz44571@gmail.com>
2023-05-29 11:50:59 +0900

    fix: 名称順番などの変更(文化部)

--- src/pages/club.tsx
+++ src/pages/club.tsx
@@ -75,23 +75,20 @@ const athletic_club: Props[] = [
   { name: 'アーチェリー部' },
 ];
 const cultural_club: Props[] = [
-  { name: '写真部', twitter: 'https://twitter.com/mct_shashinbu', instagram: 'https://www.instagram.com/mct.photos/'},
-  { name: '理科部' },
-  { name: '社会奉仕部' },
   { name: '情報科学研究部', twitter: 'https://twitter.com/mct_joken', homepage: 'https://joken.dev' },
-  { name: '囲碁将棋部', twitter: 'https://twitter.com/MCTIgoShogi', instagram: 'https://www.instagram.com/mct_shogi_club/' },
-  { name: '郷土研究部', twitter: 'https://twitter.com/kyoudoken', instagram: 'https://www.instagram.com/mct.kyodo_club/' },
-  { name: 'ロボコン部', twitter: 'https://twitter.com/Matsue_Robocon' },
-  { name: 'エコラン' },
-  { name: '吹奏楽部', twitter: 'https://twitter.com/mctbb_prm', facebook: 'https://www.facebook.com/mctwind/', instagram: 'https://www.instagram.com/mct_windclub/' },
+  { name: '吹奏楽部', twitter: 'https://twitter.com/mctbb_prm', instagram: 'https://www.instagram.com/mct_windclub/' },
   { name: '文芸部' },
-  { name: '書道部' },
-  { name: 'ESS部' },
+  { name: '写真部', twitter: 'https://twitter.com/mct_shashinbu', instagram: 'https://www.instagram.com/mct.photos/'},
+  { name: '郷土研究部', twitter: 'https://twitter.com/kyoudoken', instagram: 'https://www.instagram.com/mct.kyodo_club/' },
+  { name: '囲碁・将棋部', twitter: 'https://twitter.com/MCTIgoShogi', instagram: 'https://www.instagram.com/mct_shogi_club/' },
+  { name: '美術部' },
   { name: '漫画研究同好会', twitter: 'https://twitter.com/manken_mct'},
-  { name: '美術同好会' },
+  { name: '理科部' },
+  { name: '書道部' },
   { name: '数学同好会' },
+  { name: 'クイズ研究会', instagram: 'https://www.instagram.com/mctquiz/', twitter: 'https://twitter.com/MCT_Quiz'},
+  { name: 'ロボコン', twitter: 'https://twitter.com/Matsue_Robocon' },
   { name: 'レスコン', twitter: 'https://twitter.com/MCTrescon', homepage: 'https://mctrescon.blogspot.com/2017/05/what-is-mct-mct-mct-matsue-college-of.html?m=1', },
   { name: 'デザコン', twitter: 'https://twitter.com/mct_dezakon'},
-  { name: 'クイズ研究会', instagram: 'https://www.instagram.com/mctquiz/', twitter: 'https://twitter.com/MCT_Quiz'}
-
+  { name: 'エコラン' },
 ]
