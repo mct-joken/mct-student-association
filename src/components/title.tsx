5378e6eb3c64e212173fa16eab262c38e03e0c23
Tufusa <louptung@gmail.com>
2023-06-15 11:28:47 +0900

    fix: titleのスタイリング "text-align: center"を設定

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,15 +1,9 @@
-import React from "react";
+import styles from '../css/title.module.css';
 
-export function Title(props: { title: string }) {
+export const Title = (props: { title: string }) => {
   return (
-    <div className="title">
-      <h2 style={{
-        marginTop: "2rem",
-        borderBottom: "1px solid #000",
-        width: "100%",
-        fontWeight: "normal",
-        maxWidth: "1600px",
-      }}>{props.title}</h2>
+    <div className={styles.title}>
+      <h2 className={styles['title-text']}>{props.title}</h2>
     </div>
   );
-}
+};
